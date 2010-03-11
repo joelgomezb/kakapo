cd568cd538c1c60eca8bd169d95f915ad5383fc6
Joel Gomez <joelgomezb@gmail.com>
2010-03-11 02:32:57 -0430

    módulo donde están las principales sub-rutinas

---
+++ library.pm
@@ -0,0 +1,35 @@
+#!/usr/bin/perl 
+use Speech::Synthesis;
+
+package rutinas;
+
+sub adios {
+    	my $engine = 'Festival';
+	my @voices = Speech::Synthesis->InstalledVoices( engine => $engine );
+	my @avatars = Speech::Synthesis->InstalledAvatars( engine => $engine );
+	
+        my $ss = Speech::Synthesis->new(
+					engine => 'Festival', 
+					avatar => undef, 
+					language => 'es', 
+					voice => 'JAndalucia_Indisys_MP_es_sf_diphone', 
+					async => 0, 
+				       );
+	    $ss->speak( $_[0] || "test" );
+}
+
+sub tts {
+	my $engine = 'Festival';
+	my @voices = Speech::Synthesis->InstalledVoices( engine => $engine );
+	my @avatars = Speech::Synthesis->InstalledAvatars( engine => $engine );
+	
+        my $ss = Speech::Synthesis->new(
+					engine => 'Festival', 
+					avatar => undef, language => 'es', 
+					voice => 'JAndalucia_Indisys_MP_es_sf_diphone', 
+					async => 0, 
+				       );
+	    $ss->speak( $_[0] || "test" );
+}
+
+1;
