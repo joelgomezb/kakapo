096727d53a4242ba4436f2fee2cd7630d09375b2
Joel Enrique Gomez Brito <jgomez@sp4-adde.mppee.gob.ve>
2012-03-29 10:29:09 -0430

    modificacion de la estructura de directorio

--- lib/Kakapo.pm
+++ lib/Kakapo.pm
@@ -1,8 +1,14 @@
 package Kakapo;
 
-use 5.006;
 use strict;
 use warnings;
+use Gtk2 '-init';
+use Gtk2::Ex::Simple::List;
+use Gtk2::GladeXML;
+use Speech::Synthesis;
+use Config::General;
+use File::Slurp;
+use base qw( Gtk2::GladeXML::Simple );
 
 =head1 NAME
 
@@ -39,6 +45,50 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =cut
 
+sub new {
+  my $class = shift;
+  my $self = $class->SUPER::new( File::Spec->rel2abs(File::Spec->curdir()) . '/resources/main.glade' );
+  return $self;
+}
+
+sub run {
+  my ( $self ) = @_;
+#  $self->{vta_login}->show;
+  $self->{ventana_principal}->show;
+  Gtk2->main;
+}
+
+#my ( $programa, $ventana_principal, $vista_text, $file, $buffer_file, $texto, $voz );
+
+#	$programa 		= 	Gtk2::GladeXML->new('main.glade');
+#	$ventana_principal 	= 	$programa->get_widget('ventana_principal');
+#	$file 			= 	$programa->get_widget('filechooserbutton1');
+#	$vista_text 		= 	$programa->get_widget('textview1');
+#	$voz	 		= 	$programa->get_widget('combobox1');
+#	$programa->signal_autoconnect_from_package('main');
+
+#Gtk2->main;
+
+#sub on_bcerrar_clicked {
+#	rutinas::adios("Adios mi amigo");
+#        Gtk2->main_quit;
+#}
+
+#sub on_hablar_clicked {
+#	rutinas::tts($texto);
+#}
+
+#sub on_filechooserbutton1_file_set {
+#	$buffer_file = Gtk2::TextBuffer->new;
+#	my $archivo_seleccionado = $file->get_filename;
+#	open(LISTA, $archivo_seleccionado) || die(rutinas::tts("No se pudo abrir el archivo"));
+#	while(<LISTA>){
+#		$buffer_file->insert_at_cursor($_);
+#		$texto .= $_;
+#	}
+#	close(LISTA);	
+#	$vista_text->set_buffer($buffer_file);
+#}
 sub function1 {
 }
 
---
+++ scripts/main.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl 
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use Kakapo;
+
+Kakapo->new->run;
